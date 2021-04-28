1b9bb5e9ff938e4011c8382fbef92cc7d710cac1
Saad Srabon <57689954+saadsrabon@users.noreply.github.com>
2021-04-29 03:15:23 +0600

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+jQuery("#vmap").vectorMap({
+    map: "australia_en",
+    backgroundColor: null,
+    color: "#012169",
+    hoverColor: "#e4002b",
+    enableZoom: false,
+    selectedColor: '#e4002b',
+});
